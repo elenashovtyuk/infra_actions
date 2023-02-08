a9ed64ddd3efc018298948734a947e70108ea7ba
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-08 18:08:35 +0300

    add changes in tests.py

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -9,10 +9,10 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get('')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get('second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
